d2fc5ef55b85c5773ab3b738fd695d845140e279
Varins <marinsans001@gmail.com>
2022-06-07 18:52:18 +0200

    dodanie statistic

---
+++ src/Statistic/monitors/MonitoredVar.java
@@ -0,0 +1,51 @@
+package Statistic.monitors;
+
+/**
+ * Klasa obiektów gromadzących wartości obserwowane (monitorowane) w symulacji. Zbiera dane jako szeregi czasowe obiektów 'Change' w kolekcji 'ChangesList'. 
+ * 
+ * @author Dariusz Pierzchala
+ *
+ */
+public class MonitoredVar {
+
+	private Histogram histogram;
+	private ChangesList changes;
+	private double actualVal;	
+
+	/**
+	 * Konstruktor obiektu monitorującego.  
+	 */
+	public MonitoredVar() {
+		this.changes = new ChangesList();
+	}
+
+	/**
+	 * Dodaje nową wartość do szeregu obserwacji gromadzonych w monitorze. 
+	 * @param newValue Nowa wartość monitorowana
+	 * @param simTime Wartość czasu symulacyjnego zmiany
+	 */
+	public void setValue(double newValue, double simTime) {
+		this.changes.add(new Change(newValue, simTime));
+		this.actualVal = newValue;
+	}
+
+	public double getValue() {
+		return actualVal;
+	}
+
+	public Histogram getHistogram() {
+//		if (histogram == null)
+//			histogram = new Histogram(this);
+//		return histogram;
+		return new Histogram(this);
+	}
+
+	public ChangesList getChanges() {
+		return changes;
+	}
+
+	public int numberOfSamples() {
+		return changes.size();
+	}
+
+}
